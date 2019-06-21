c3dc8e3f7d914097d5d041fad34da2500a410812
Travis CI <travis@travis-ci.org>
2019-06-21 14:59:00 +0000

    [ci skip] travis is OK: v2.1.22 - 0d47656

--- dist/esm5/version/index.js
+++ dist/esm5/version/index.js
@@ -3,6 +3,6 @@
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-export var version = '2.1.21';
+export var version = '2.1.22';
 
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoibmc6Ly9maWRqLyIsInNvdXJjZXMiOlsidmVyc2lvbi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUNBLFdBQWEsT0FBTyxHQUFHLFFBQVEsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGJ1bXBlZCB2ZXJzaW9uIHZpYSBndWxwXG5leHBvcnQgY29uc3QgdmVyc2lvbiA9ICcyLjEuMjEnO1xuIl19
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290Ijoibmc6Ly9maWRqLyIsInNvdXJjZXMiOlsidmVyc2lvbi9pbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7OztBQUNBLFdBQWEsT0FBTyxHQUFHLFFBQVEsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbIi8vIGJ1bXBlZCB2ZXJzaW9uIHZpYSBndWxwXG5leHBvcnQgY29uc3QgdmVyc2lvbiA9ICcyLjEuMjInO1xuIl19
--- dist/version/index.d.ts
+++ dist/version/index.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "2.1.21";
+export declare const version = "2.1.22";
