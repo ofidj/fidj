b5e7d6891beee7e37e35fd8a9d04f3cdffb747f1
Travis CI <travis@travis-ci.org>
2020-09-04 04:50:35 +0000

    [ci skip] travis is OK: v2.1.40 - 3b43264

--- dist/fesm2015/fidj.js
+++ dist/fesm2015/fidj.js
@@ -251,7 +251,7 @@ var LoggerLevelEnum;
 })(LoggerLevelEnum || (LoggerLevelEnum = {}));
 
 // bumped version via gulp
-const version = '2.1.39';
+const version = '2.1.40';
 
 // import {XHRPromise} from './xhrpromise';
 // const superagent = require('superagent');
--- dist/version/index.d.ts
+++ dist/version/index.d.ts
@@ -1,1 +1,1 @@
-export declare const version = "2.1.39";
+export declare const version = "2.1.40";
